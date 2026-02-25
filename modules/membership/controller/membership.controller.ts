bf76444334514642e6c54704c5f41e7b9519a39b
Facundo Robert <robert.facundo@hotmail.com>
2026-02-25 11:35:46 -0300

    logs membership controller

--- modules/membership/controller/membership.controller.ts
+++ modules/membership/controller/membership.controller.ts
@@ -3,7 +3,9 @@ import { createMembershipSession } from '../services/membership.service';
 
 export const createMembershipController = async (req: Request, res: Response) => {
     try {
+        console.log(req.body);
         const { userId, successUrl, cancelUrl, priceId } = req.body;
+        console.log({ userId, priceId, userEmail: user.email });
         const session = await createMembershipSession({ userId, successUrl, cancelUrl, priceId });
         res.json(session);
     } catch (err: any) {
